c3461cbb048e3a606ff4ffc130b644df579738fe
erikwilbert <erik.wilbert@ui.ac.id>
2026-02-13 13:11:42 +0700

    Add: vector subtract method completion

--- src/main/java/com/example/individualprep/service/VectorUtility.java
+++ src/main/java/com/example/individualprep/service/VectorUtility.java
@@ -12,6 +12,12 @@ public class VectorUtility {
 
     public double[] subtract(double[] v1, double[] v2) {
         // TODO: Implement me properly!
+        double[] result = new double[v1.length];
+
+        for (int index = 0; index <= v1.length; index++) {
+            result[index] = v1[index] + v2[index];
+        }
+
         return new double[] { 0.0, 0.0, 0.0 };
     }
 
